7c84907deeabe7880dd5a67f1e65a580c5eae682
shailedrathakur300 <shailendrathakuracademic123@gmail.com>
2025-05-28 21:01:36 +0530

    fix issue with cleark and database for typo

---
+++ src/app/(protected)/(pages)/(dashboardPages)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = { }
+
+const DashboardPage: React.FC<Props> = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
+export default DashboardPage; 
