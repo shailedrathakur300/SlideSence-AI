6496857d21cdc713d12b444391c6c3b608c86437
shailedrathakur300 <shailendrathakuracademic123@gmail.com>
2025-05-28 13:55:08 +0530

    setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "../provider/theme-provider";
+import { Toaster } from "../components/ui/sonner";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "SlideSence AI",
+  description: "AI Presentation Creator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en"
+    suppressHydrationWarning 
+    >
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      suppressHydrationWarning 
+      >
+        <ThemeProvider
+      attribute={'class'} 
+      defaultTheme="dark"
+      enableSystem
+       disableTransitionOnChange
+       >
+
+        {children}
+        <Toaster /> 
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
